0dda0980003efa2facd99b40895310eca34af757
COP412L <duvan.rojas@inacapmail.cl>
2022-06-06 09:29:18 -0400

    FALTA FINALIZAR

--- app/src/main/java/com/example/ficha_project/destino.java
+++ app/src/main/java/com/example/ficha_project/destino.java
@@ -4,10 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,12 +21,18 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
-
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.navigation.NavigationView;
 
-public class destino extends AppCompatActivity {
+public class destino extends AppCompatActivity implements GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener, OnMapReadyCallback, ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean permissionDenied = false;
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +64,9 @@ public class destino extends AppCompatActivity {
                 }
             }
         });*/
+        //                                          REFERENCIA GOOGLE MAPS API
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
         //                              BOTTOM APP BAR (MENÚ INFERIOR)
         final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.navi));
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
@@ -85,20 +100,22 @@ public class destino extends AppCompatActivity {
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
 
             }
+
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
                 //Color baseColor = new Color(); //= Color.BLACK;
 
                 float baseAlpha = ResourcesCompat.getFloat(getResources(), com.google.android.material.R.dimen.material_emphasis_medium);
-                float offset = (slideOffset-(-1f))/(1f-(-1f))*(1f-0f)+0f;
-                float alpha = com.google.android.material.math.MathUtils.lerp(0f,255f,offset*baseAlpha);
+                float offset = (slideOffset - (-1f)) / (1f - (-1f)) * (1f - 0f) + 0f;
+                float alpha = com.google.android.material.math.MathUtils.lerp(0f, 255f, offset * baseAlpha);
                 int a = (int) alpha;
-                int color = Color.argb(a,0,0,0);
+                int color = Color.argb(a, 0, 0, 0);
                 scrim.setBackgroundColor(color);
             }
         });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //Incoporar menú dentro de Activity
@@ -107,14 +124,44 @@ public class destino extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
-        if (id==R.id.opPerfil){
+        if (id == R.id.opPerfil) {
             Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(getApplicationContext(),Perfil.class);
+            Intent i = new Intent(getApplicationContext(), Perfil.class);
             startActivity(i);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        map.setOnMyLocationButtonClickListener(this);
+        map.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+            return;
+        }
     }
 }
