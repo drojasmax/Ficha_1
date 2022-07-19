f88fab31435839da1613e1c6b7fae0fe84e522c4
DUVAN <duvan_maximiliano@hotmail.com>
2022-07-19 18:23:58 -0400

    IMPLEMENTACIÓN DE LA API DE PLACES AUTOCOMPLETE HECHA

--- app/src/main/java/com/example/ficha_project/Perfil.java
+++ app/src/main/java/com/example/ficha_project/Perfil.java
@@ -1,23 +1,9 @@
 package com.example.ficha_project;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import Modelo.Conductor;
-import Modelo.Cuenta;
-import Modelo.Pasajero;
 
 public class Perfil extends AppCompatActivity {
 
