eaa90a718dc7096d043fc5555e5b7a77c65aa9d8
DanDie <jonatanfuentes236@gmail.com>
2022-07-22 00:22:23 -0400

    LOGIN CONECTADO A BD

--- app/src/main/java/com/example/ficha_project/Login.java
+++ app/src/main/java/com/example/ficha_project/Login.java
@@ -1,5 +1,7 @@
 package com.example.ficha_project;
 
+import com.example.ficha_project.R;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,26 +9,72 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Login extends AppCompatActivity {
 
     EditText etCorreo, etContra;
     Button btnIniciarSesion;
+    String correo,contra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etCorreo = findViewById(R.id.etCorreo);
+        etContra = findViewById(R.id.etContra);
+        btnIniciarSesion = findViewById(R.id.btnLogin);
+
+        btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarUsuario("http://200.29.219.25:3307/duvan/validarPasajero.php");
+            }
+        });
 
     }
-    public void login(View v){
-        String correo = etCorreo.getText().toString().trim();
-        String contra = etContra.getText().toString().trim();
+    public void validarUsuario (String URL) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (!response.isEmpty()) {
+                    Intent intent = new Intent(getApplicationContext(), pasajero_principal.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Login.this, "Usuario o contrasenia incorrecta", Toast.LENGTH_SHORT);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Login.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("Correo", etCorreo.getText().toString());
+                parametros.put("Contrasenia", etContra.getText().toString());
 
-
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest); //todas las peticiones de nuestra app
     }
-
-    public void crearCuenta(View v){
-        Intent i = new Intent(this, redireccionar_pasajero_conductor.class);
-        startActivity(i);}
 }
